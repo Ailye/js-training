157fb00ba54672e26f8a2573348b293567a3244d
P-Hugo <ph.meillot@gmail.com>
2018-09-18 10:09:44 +0200

    primitive

--- exercises/primitives.js
+++ exercises/primitives.js
@@ -8,10 +8,10 @@
  */
 
 // Your code :
-const str="1337";
-const num=42;
-const bool=false;
-const undef=undefined;
+const str = "1337";
+const num = 42;
+const bool = false;
+const undef = undefined;
 
 //* Begin of tests
 const assert = require('assert')
